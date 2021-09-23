1398654f2f668730a2f7b1a4155036b6ea155cf6
Saikou Ceesay <ceesaysaikou72@gmail.com>
2021-09-23 01:29:34 -0400

    added video access

--- extension/src/components/Home.js
+++ extension/src/components/Home.js
@@ -1,9 +1,29 @@
 import './Home.css';
 import { getAuth } from '@firebase/auth';
 import app from '../firebase';
+import { useRef, useEffect } from 'react';
 
 const Home = () => {
   const auth = getAuth(app);
+
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error('error:', err);
+      });
+  };
 
   const logout = () => {
     auth.signOut();
@@ -12,6 +32,8 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Welcome to Eagle Eye</h1>
+      <video ref={videoRef} />
+      <button className="home__logout">Take a photo</button>
       <button onClick={logout} type="submit" className="home__logout">
         Logout
       </button>
