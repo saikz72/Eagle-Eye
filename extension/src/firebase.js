b903f3c44b1d0df0e02bc4696c500d55f405e7fa
Saikou Ceesay <ceesaysaikou72@gmail.com>
2021-09-22 21:34:20 -0400

    authentication page set up

---
+++ extension/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD12pvdMtU-6EiMpVlmRPz4BgkCeFXs06A',
+  authDomain: 'compass-e0a50.firebaseapp.com',
+  projectId: 'compass-e0a50',
+  storageBucket: 'compass-e0a50.appspot.com',
+  messagingSenderId: '94333808784',
+  appId: '1:94333808784:web:18fe0962239de957dce815',
+  measurementId: 'G-8N2YHQT39S',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
